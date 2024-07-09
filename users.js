31ad18e90004f7e17e5f0189083a8c430b24c622
ASWATH <aswatharumugam23@gmail.com>
2024-07-10 00:28:21 +0200

    Add files via upload

---
+++ users.js
@@ -0,0 +1,80 @@
+const users = [
+    {
+      userName: {
+        firstName: "John",
+        lastName: "Smith"
+      },
+      matriculationId: "85165321",
+      email: "john.doe@tu-berlin.de",
+      dateAndTimings: "2024-06-24 14:00",
+      youtubeVideoLink: "https://www.youtube.com/watch?v=127ng7botO4",
+      place: "Berlin campus",
+      map:{
+        lat : 52.5105998,
+        long : 13.3151812,
+      }
+    },
+    {
+      userName: {
+        firstName: "Larry",
+        lastName: "Paige"
+      },
+      matriculationId: "87654321",
+      email: "jane.smith@work.de",
+      dateAndTimings: "2024-07-01 10:30",
+      youtubeVideoLink: "https://www.youtube.com/watch?v=EcCTIExsqmI",
+      place: "Munich campus",
+      map:{
+        lat : 52.5105998,
+        long : 13.3151812,
+      }
+    },
+    {
+      userName: {
+        firstName: "Morris",
+        lastName: "Johnson"
+      },
+      matriculationId: "23456789",
+      email: "alice.johnson@gisma.edu.de",
+      dateAndTimings: "2024-07-15 09:00",
+      youtubeVideoLink: "https://youtu.be/LfaMVlDaQ24",
+      place: "Potsdam campus",
+      map:{
+        lat : 52.43493,
+        long : 13.05444,
+      }
+    },
+    {
+      userName: {
+        firstName: "Elon",
+        lastName: "Musk"
+      },
+      matriculationId: "34567890",
+      email: "bob.brown@freinus-school.de",
+      dateAndTimings: "2024-08-05 13:45",
+      youtubeVideoLink: "https://www.youtube.com/watch?v=W_HYzNExtQM",
+      place: "Berlin campus",
+      map:{
+        lat : 52.51770,
+        long : 13.39656,
+      }
+    },
+    {
+      userName: {
+        firstName: "Jeff",
+        lastName: "Bezos"
+      },
+      matriculationId: "45678901",
+      email: "eve.davis@code-school.edu",
+      dateAndTimings: "2024-09-10 08:15",
+      youtubeVideoLink: "https://www.youtube.com/watch?v=OrxmtDw4pVI",
+      place: "Frankfurt campus",
+      map:{
+        lat : 52.5105998,
+        long : 13.3151812,
+      }
+    }
+  ];
+  
+  module.exports = users;
+  
